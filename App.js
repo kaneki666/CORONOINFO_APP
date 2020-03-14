84859c753da4da719b1dfc154653751e1c07fbe6
Mohammad Sadman <41444793+kaneki666@users.noreply.github.com>
2020-03-14 19:42:47 +0600

    first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import IndexPage from './Components/Corona';
+import Screen from './Components/Screen';
+
+const App = () => {
+  return (
+    <>
+      <Screen />
+    </>
+  );
+};
+
+export default App;
